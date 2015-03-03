581dc71821d7d0c8e9cd61014d25c0c64285f408
Mauris Van Hauwe <0nooodl@gmail.com>
2015-03-03 13:31:31 +0100

    Draw window borders (2)

--- ram/view.py
+++ ram/view.py
@@ -9,4 +9,15 @@ def update_screen(stdscr):
     win_text = curses.newwin(WIN_TEXT_HEIGHT, WIN_TEXT_WIDTH,
                              WIN_TEXT_Y, WIN_TEXT_X)
 
+    HL, VL = curses.ACS_HLINE, curses.ACS_VLINE
+    UL, UR = curses.ACS_ULCORNER, curses.ACS_URCORNER
+    LL, LR = curses.ACS_LLCORNER, curses.ACS_LRCORNER
+    SP = ord(' ')
 
+    win_status.border(VL, VL, HL, HL, UL, UR, curses.ACS_LTEE)
+    win_status.refresh()
+    win_game.border(VL, VL, HL, HL, UL, UR, LL, curses.ACS_RTEE)
+    win_game.refresh()
+    win_text.border(VL, VL, SP, HL, VL, VL)
+    win_text.refresh()
+    win_text.getch()
