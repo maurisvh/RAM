45fcd95846c98df419cd80cf3003bbfe7eda8356
Mauris Van Hauwe <0nooodl@gmail.com>
2015-03-03 13:31:43 +0100

    Move init_pair calls to Color.py

--- ram/Color.py
+++ ram/Color.py
@@ -5,6 +5,16 @@ def initialize():
     global BLUE, GREEN, CYAN, RED, MAGENTA, BROWN, LIGHTGRAY
     global DARKGRAY, LIGHTBLUE, LIGHTGREEN, LIGHTCYAN, LIGHTRED
     global LIGHTMAGENTA, YELLOW, WHITE
+
+    curses.init_pair(1, curses.COLOR_BLUE,    curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN,   curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_CYAN,    curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_RED,     curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_YELLOW,  curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_WHITE,   curses.COLOR_BLACK)
+    curses.init_pair(8, curses.COLOR_BLACK,   curses.COLOR_BLACK)
+
     BLUE         = curses.color_pair(1)
     GREEN        = curses.color_pair(2)
     CYAN         = curses.color_pair(3)
--- ram/ram.py
+++ ram/ram.py
@@ -4,32 +4,20 @@ import Color
 from Player import Player
 from messages import messages
 import memory
-
-def init_color_pairs():
-    """Initializes color pairs for curses."""
-    # These numbers are used by Color.blue, etc.
-    curses.init_pair(1, curses.COLOR_BLUE,    curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN,   curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_CYAN,    curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_RED,     curses.COLOR_BLACK)
-    curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-    curses.init_pair(6, curses.COLOR_YELLOW,  curses.COLOR_BLACK)
-    curses.init_pair(7, curses.COLOR_WHITE,   curses.COLOR_BLACK)
-    curses.init_pair(8, curses.COLOR_BLACK,   curses.COLOR_BLACK)
+import view
 
 def main(stdscr):
     """Starts the game."""
     curses.initscr()
-    init_color_pairs()
     Color.initialize()
 
     player = Player()
-    memory.write_memory(player, memory.addr_player_appearance, 41)
-    memory.write_memory(player, memory.addr_player_appearance, 41+64)
-    for i, line in enumerate(messages):
-        stdscr.addstr(i, 0, line[0])
-    stdscr.addstr(9, 9, player.char, player.color)
-    stdscr.getch()
+    # memory.write_memory(player, memory.addr_player_appearance, 41)
+    # memory.write_memory(player, memory.addr_player_appearance, 41+64)
+    # for i, line in enumerate(messages):
+    #     stdscr.addstr(i, 0, line[0])
+    # stdscr.addstr(9, 9, player.char, player.color)
+    view.update_screen(stdscr)
 
 if __name__ == '__main__':
     curses.wrapper(main)
