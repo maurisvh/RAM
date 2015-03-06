c496c87404ad86f7338138adb2231be461b9881c
Mauris Van Hauwe <0nooodl@gmail.com>
2015-03-06 12:44:06 +0100

    Add Element name function

--- ram/Element.py
+++ ram/Element.py
@@ -2,3 +2,15 @@ METAL = 'metal'
 ACID = 'acid'
 FIRE = 'fire'
 ELEC = 'elec'
+
+def name(element, verbose=False):
+    if element == METAL:
+        return 'metal'
+    elif element == ACID:
+        return 'acid'
+    elif element == FIRE:
+        return 'fire'
+    elif element == ELEC:
+        return 'electricity' if verbose else 'elec'
+    else:
+        raise ValueError('expected Element')
