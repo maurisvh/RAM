0ef6d10b2c63104bd2673f51ec3ad15dbca4f633
Mauris Van Hauwe <0nooodl@gmail.com>
2015-03-11 21:33:29 +0100

    Map updates in view

--- ram/view.py
+++ ram/view.py
@@ -37,6 +37,7 @@ WIN_GAME_BOX_HEIGHT = WIN_STATUS_HEIGHT
 def update_status(win_status, player):
     caption_colour = Color.DARKGRAY
     value_colour = Color.LIGHTGRAY
+    win_status.addstr(0, 0, '!', Color.BLACK)
 
     def ratio_color(r):
         if r > 0.8:
@@ -78,8 +79,8 @@ def update_status(win_status, player):
 
     show_hp = writes_ratio('HP: ',player.hp, player.maxhp)
     show_tp = writes_ratio('TP: ',player.tp, player.maxtp)
-    show_dlvl = writes_value('Dlvl: ', player.dlvl)
-    show_xl   = writes_value('XL: ',   player.xl)
+    show_dlvl = writes_value('Depth: ', player.dlvl)
+    show_xl   = writes_value('Exp: ',   player.xl)
     show_def  = writes_value('Def: ',  player.defense)
 
     def show_apt(element):
@@ -106,19 +107,38 @@ def update_status(win_status, player):
 
     # Put the RAM table right below the HUD.
     # XXX: what about items? other dialog boxes?
-    ram_y = y + 2
-    ram_width = 8 * 3 - 1
-    ram_x = (WIN_STATUS_WIDTH - ram_width) // 2
-    for i in range(0x40):
-        y = ram_y + i // 8
-        x = ram_x + i % 8 * 3
-        byte = random.randint(0, 255)
-        attr = Color.CYAN
-        if i == player.address:
-            attr = Color.LIGHTGRAY | curses.A_REVERSE
-        win_status.addstr(y, x, "{:02X}".format(byte), attr)
+
+    if not player.show_ram:
+        ram_y = y + 2
+        ram_width = 8 * 3 - 1
+        ram_x = (WIN_STATUS_WIDTH - ram_width) // 2
+        for i in range(0x40):
+            y = ram_y + i // 8
+            x = ram_x + i % 8 * 3
+            byte = random.randint(0, 255)
+            attr = Color.CYAN
+            if i == player.address:
+                attr = Color.LIGHTGRAY | curses.A_REVERSE
+            win_status.addstr(y, x, "{:02X}".format(byte), attr)
+    else:
+        item_y = y + 2
+        for i in range(8):
+            y = item_y + i
+            item = player.inventory[i]
+            line = item.compressed_name() if item.kind else 'nothing'
+            num_color = Color.LIGHTBLUE if item.kind else Color.DARKGRAY
+            line_color = item.color() if item.kind else Color.DARKGRAY
+            win_status.addstr(y, 2, '{0} '.format(i + 1), num_color)
+            win_status.addstr(line, line_color)
 
     win_status.refresh()
+
+def update_map(win_game, player, dungeon):
+    board = dungeon[player.dlvl]
+    for y in range(Board.HEIGHT):
+        for x in range(Board.WIDTH):
+            win_game.insstr(y, x, board.char_at(x, y), board.color_at(x, y))
+    win_game.refresh()
 
 def update_screen(stdscr, player, dungeon):
     win_status = curses.newwin(WIN_STATUS_HEIGHT, WIN_STATUS_WIDTH,
@@ -131,23 +151,25 @@ def update_screen(stdscr, player, dungeon):
     win_text = curses.newwin(WIN_TEXT_HEIGHT, WIN_TEXT_WIDTH,
                              WIN_TEXT_Y, WIN_TEXT_X)
 
-    win_status.border()
+    #win_status.border()
     win_status.refresh()
-    win_game_box.bkgdset('#', Color.DARKGRAY)
-    win_game_box.clear()
+    for y in range(WIN_GAME_BOX_HEIGHT):
+        for x in range(WIN_GAME_BOX_WIDTH):
+            win_game_box.insstr(y, x, '\u2571', Color.BACKGROUND)
     win_game_box.refresh()
     win_game.clear()
     win_game.refresh()
 
     update_status(win_status, player)
+    update_map(win_game, player, dungeon)
 
-    stdscr.clear()
-    for i in range(0x20):
-        it = Item.Item(i << 3)
-        stdscr.addstr(i % 16 + 3, i // 16 * 25 + 17,
-                it.char() + ' ', it.color())
-        for c in it.kind_name():
-            stdscr.addstr(c, it.color())
-    stdscr.refresh()
+    #stdscr.clear()
+    #for i in range(1,0x20):
+    #    it = Item.Item(i << 3)
+    #    stdscr.addstr(i % 16 + 3, i // 16 * 25 + 17,
+    #            it.char() + ' ', it.color())
+    #    for c in it.kind_name():
+    #        stdscr.addstr(c, it.color())
+    #stdscr.refresh()
 
     stdscr.getch()
